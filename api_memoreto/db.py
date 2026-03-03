8b77b1dbeac9cca6b5c4ab0c6983fd00a681bdb2
IrvingRosas73 <A01803057@tec.mx>
2026-03-03 09:57:09 -0600

    Update db.py

--- api_memoreto/db.py
+++ api_memoreto/db.py
@@ -4,7 +4,7 @@ from flask import current_app, g
 def get_db():
     if 'db' not in g:
         g.db = sqlite3.connect(
-            'db_memoreto.sqlite', # Conecta a la BD que generó tu script
+            'db_memoreto.sqlite',
             detect_types=sqlite3.PARSE_DECLTYPES
         )
         g.db.row_factory = sqlite3.Row
