63f8d081b4ba621e3114081009b137f3ffafb365
CamilaQuintanilla11 <camilaquintanillacabrera@gmail.com>
2026-04-06 17:02:15 -0600

    prueba web

--- api_memoreto/setup_db.py
+++ api_memoreto/setup_db.py
@@ -1,28 +1,25 @@
 import sqlite3
+import os
 
 def inicializar_bd():
-    # Conecta o crea el archivo de la base de datos
-    conexion = sqlite3.connect('db_memoreto.sqlite')
+
+    DB_PATH = os.path.join(os.path.dirname(__file__), "db_memoreto.sqlite")
+
+    conexion = sqlite3.connect(DB_PATH)
     cursor = conexion.cursor()
-    
-    # Lee el archivo SQL con las tablas
-    with open("api_memoreto/tablas_memoreto.sql", "r", encoding="utf-8", errors="ignore") as archivo_sql:
+
+    sql_path = os.path.join(os.path.dirname(__file__), "tablas_memoreto.sql")
+
+    with open(sql_path, "r", encoding="utf-8", errors="ignore") as archivo_sql:
         script = archivo_sql.read()
-        
-    # Ejecuta el script para crear las tablas
+
     cursor.executescript(script)
-    
-    # Inserta el usuario de prueba de su documentación (Endpoint 1)
-    # Nota: En un proyecto real la contraseña iría encriptada (hash), 
-    # pero para esta fase de pruebas la dejamos en texto plano.
-    #cursor.execute("""
-     #   INSERT INTO usuario (correo, password, rol) 
-      #  VALUES ('alumnoabc@tec.mx', '12345678', 'estudiante')
-    #""")
-    
+
     conexion.commit()
     conexion.close()
-    print("¡Base de datos 'db_memoreto.sqlite' inicializada correctamente con el usuario de prueba!")
+
+    print("DB creada correctamente en:", DB_PATH)
+
 
 if __name__ == '__main__':
     inicializar_bd()
